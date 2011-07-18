b646196a6aafa426702db668d100b8c65f98c98b
palewire <ben.welsh@gmail.com>
2011-07-18 10:02:58 -0700

    Ported listtables management command that will tell you which YAMLs are in the database. Fixes #4.

--- table_stacker/management/commands/listtables.py
+++ table_stacker/management/commands/listtables.py
@@ -1,16 +1,31 @@
+from toolbox import ptable
 from django.conf import settings
 from google.appengine.ext import db
+from table_stacker.management.commands import *
 from toolbox.FileIterator import FileIterator
 from django.core.management.base import BaseCommand, CommandError
 
 
-class Command(BaseCommand):
-    help = 'Print the names of all tables configured in the YAML repository'
+class Command(GAECommand):
+    help = 'Report on all the tables configured in the YAML repository'
     
     def handle(self, *args, **options):
-        file_list = FileIterator(settings.YAML_DIR)
-        yaml_list = [i for i in file_list if i.endswith('.yaml')]
-        yaml_list.sort()
-        for i in yaml_list:
-            print i.replace(".yaml", "")
-
+        
+        # Login
+        self.authorize(options)
+        
+        results = []
+        for i in get_all_yaml():
+            obj = Table.get_by_key_name(i.get("slug"))
+            if obj:
+                exists = 'Y'
+            else:
+                exists = 'N'
+            results.append([i.get("yaml_name"), exists])
+        results.sort(key=lambda x: x[0])
+        print ptable.indent(
+            [['yaml', 'exists']] + results, 
+            hasHeader=True, 
+            separateRows=False,
+            prefix='| ', postfix=' |',
+        )
