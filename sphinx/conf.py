f8f9c1a577fc3bdd3691ada8ba2fe511616d69b2
palewire <ben.welsh@gmail.com>
2011-08-07 14:47:15 -0700

    Fixed github link

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Table Stacker'
+project = u'latimes-table-stacker'
 copyright = u'2011, Los Angeles Times'
 
 # The version info for the project you're documenting, acts as replacement for
