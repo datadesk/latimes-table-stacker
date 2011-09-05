f6bd60571e612ea6cdd5724f611a0a1270fa5446
palewire <ben.welsh@gmail.com>
2011-09-04 21:11:31 -0700

    Cleared out some crap we don't need. Beef up build command will all the biz it uses, since there's no duplication anymore.

--- table_stacker/management/commands/build.py
+++ table_stacker/management/commands/build.py
@@ -1,26 +1,74 @@
 import os
 import math
+import yaml
 import shutil
+from table_stacker import views
 from django.http import Http404
 from django.conf import settings
-from table_stacker import views
 from table_stacker.models import Table, Tag
-from table_stacker.management.commands import *
+from table_stacker.feeds import LatestTables
+from toolbox.FileIterator import FileIterator
 from django.test.client import RequestFactory
 from django.contrib.syndication.views import feed
-from django.views.generic.simple import direct_to_template
-from table_stacker.feeds import LatestTables
 from django.core.management.base import BaseCommand
+
+
+class YAMLDoesNotExistError(Exception):
+    """
+    Called when you try to open a YAML that doesn't exist.
+    """
+    def __init__(self, value):
+        self.parameter = value
+    
+    def __str__(self):
+        return repr(self.parameter)
+
+
+class InvalidYAMLError(Exception):
+    """
+    Called when your YAML doesn't get with the program.
+    """
+    def __init__(self, value):
+        self.parameter = value
+    
+    def __str__(self):
+        return repr(self.parameter)
 
 
 class Command(BaseCommand):
     help = 'Bake out the entire site as flat files in the build directory'
     
+    def get_yaml(self, yaml_name):
+        """
+        Retrieves the yaml file with the provided name as a Python object.
+        """
+        yaml_path = os.path.join(settings.YAML_DIR, yaml_name)
+        try:
+            yaml_data = open(yaml_path)
+        except:
+            raise YAMLDoesNotExistError("YAML file could not be opened: %s" % yaml_name)
+        try:
+            yaml_obj = yaml.load(yaml_data)['table']
+            yaml_obj['yaml_name'] = yaml_name
+        except:
+            raise InvalidYAMLError("YAML file is improperly formatted.")
+        yaml_data.close()
+        return yaml_obj
+    
+    def get_all_yaml(self):
+        """
+        Returns a list of all the tables configured in the YAML_DIR
+        in dictionary form.
+        """
+        file_list = FileIterator(settings.YAML_DIR)
+        yaml_list = [i for i in file_list if i.endswith('.yaml')]
+        return [self.get_yaml(i) for i in yaml_list]
+    
     def write(self, path, data):
         outfile = open(os.path.join(settings.BUILD_DIR, path), 'w')
         outfile.write(data)
         outfile.close()
-
+    
     def handle(self, *args, **options):
         
         self.stdout.write("Creating build directory\n")
@@ -38,7 +86,7 @@ class Command(BaseCommand):
         
         # Load all YAML files into the local database
         self.stdout.write("Building database\n")
-        [Table.objects.update_or_create(i) for i in get_all_yaml()]
+        [Table.objects.update_or_create(i) for i in self.get_all_yaml()]
         
         # Create a fake request we can use to fire up the pages
         rf = RequestFactory()
