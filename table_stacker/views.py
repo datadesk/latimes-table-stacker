79884d4096d4123b373cd7872f51c4f42f770f85
palewire <ben.welsh@gmail.com>
2012-01-12 19:02:45 -0800

    Added some docstrings and simplified things a touch

--- table_stacker/views.py
+++ table_stacker/views.py
@@ -15,13 +15,17 @@ class TableListView(ListView):
     queryset = Table.live.all()
     
     def build(self):
+        """
+        Build the view as a flat HTML file.
+        """
+        # Make a fake request
         self.request = RequestFactory().get("/")
+        # Render the list page as HTML
         html = self.get(self.request).render().content
-        self.write('index.html', html)
-    
-    def write(self, path, data):
-        outfile = open(os.path.join(settings.BUILD_DIR, path), 'w')
-        outfile.write(data)
+        # Write it out to the appointed flat file
+        path = os.path.join(settings.BUILD_DIR, 'index.html')
+        outfile = open(path, 'w')
+        outfile.write(html)
         outfile.close()
 
 
@@ -41,21 +45,29 @@ class TableDetailView(DetailView):
         return context
     
     def build_object(self, obj):
+        """
+        Build a detail page as a flat HTML file.
+        """
+        # Make a fake request
         self.request = RequestFactory().get("/%s/" % obj.slug)
-        self.kwargs = {'slug': obj.slug}
+        # Set the kwargs to fetch this particular object
+        self.kwargs = dict(slug=obj.slug)
+        # Render the detail page HTML
         html = self.get(self.request).render().content
+        # Create the path to save the flat file
         path = os.path.join(settings.BUILD_DIR, obj.slug)
-        self.write(path, html)
-
-    def build_queryset(self):
-        [self.build_object(obj) for obj in self.queryset]
-    
-    def write(self, path, data):
         os.path.exists(path) or os.makedirs(path)
         path = os.path.join(path, 'index.html')
-        outfile = open(os.path.join(settings.BUILD_DIR, path), 'w')
-        outfile.write(data)
+        # Write out the data
+        outfile = open(path, 'w')
+        outfile.write(html)
         outfile.close()
+    
+    def build_queryset(self):
+        """
+        Build flat HTML files for all of the objects in the queryset.
+        """
+        [self.build_object(obj) for obj in self.queryset]
 
 
 def sitemap(request):
