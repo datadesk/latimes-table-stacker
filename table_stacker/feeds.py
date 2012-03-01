27bdb0aaec720c6e0ecb6521e7626b0009d98eec
palewire <ben.welsh@gmail.com>
2012-02-29 17:07:39 -0800

    Got latest feed to come out in XML

--- table_stacker/feeds.py
+++ table_stacker/feeds.py
@@ -6,3 +6,9 @@ class LatestTablesFeed(BuildableListView):
     build_path = 'feeds/latest.xml'
     template_name = 'table_stacker/feeds/latest.xml'
     queryset = Table.live.all()[:10]
+    
+    def render_to_response(self, context):
+        return super(LatestTablesFeed, self).render_to_response(
+            context,
+            content_type='text/xml'
+        )
