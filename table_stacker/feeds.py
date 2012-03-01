633c837ceaabf608a93ba26b7efaba5ee5bb67e8
palewire <ben.welsh@gmail.com>
2012-02-29 17:05:44 -0800

    Removed commented out

--- table_stacker/feeds.py
+++ table_stacker/feeds.py
@@ -6,9 +6,3 @@ class LatestTablesFeed(BuildableListView):
     build_path = 'feeds/latest.xml'
     template_name = 'table_stacker/feeds/latest.xml'
     queryset = Table.live.all()[:10]
-    
-#    def items(self):
-#        return self.queryset
-#    
-#    def item_pubdate(self, item):
-#        return datetime(*(item.publication_date.timetuple()[:6]))
